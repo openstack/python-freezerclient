7b10fd4b61bc6a08a3057e8b67cb8778585513d1
jacky06 <zhang.min@99cloud.net>
2020-04-10 13:34:34 +0800

    Use unittest.mock instead of third party mock
    Now that we no longer support py27, we can use the standard library
    unittest.mock module instead of the third party mock lib.
    
    Change-Id: I880d1d8d6183d002b81ac34e2f2a3b2625abb4ff

--- freezerclient/tests/unit/v1/test_client_backups.py
+++ freezerclient/tests/unit/v1/test_client_backups.py
@@ -13,8 +13,7 @@
 # limitations under the License.
 
 import unittest
-
-import mock
+from unittest import mock
 
 from freezerclient import exceptions
 from freezerclient.v1.managers import backups
